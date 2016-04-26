33b56afb42359d18b516eb93b221cbb41250c933
Andrés Mata Suárez <amatasuarez@gmail.com>
2016-04-26 02:28:24 -0300

    Added security layer with helmet

--- server/app/app_config.js
+++ server/app/app_config.js
@@ -7,6 +7,7 @@ const morgan       = require('morgan');
 const compression  = require('compression');
 const bodyparser   = require('body-parser');
 const favicon      = require('serve-favicon');
+const helmet       = require('helmet');
 const Middlewares  = require('../middlewares');
 const Admin        = require('../model/admin');
 
@@ -29,6 +30,31 @@ module.exports = function(app) {
   app.use(cookieParser());
   app.use(passport.initialize());
   app.use(Middlewares.TokenExtractor);
+
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+  app.use(helmet.contentSecurityPolicy({
+    defaultSrc : [ "'self'" ],
+    scriptSrc  : [
+      "'unsafe-inline'",
+      "'self'",
+      "'unsafe-eval'",
+      '*.google.com',
+      '*.gstatic.com',
+      '*.google-analytics.com'
+    ],
+    styleSrc   : [ "'unsafe-inline'", "'self'", '*.fonts.googleapis.com' ],
+    imgSrc     : [ '*' ], // "'self'", '*.google.com', '*.cloudinary.com'
+    connectSrc : [ "'self'" ],
+    fontSrc    : [ "'self'", '*.fonts.gstatic.com' ],
+    objectSrc  : [ "'self'" ],
+    mediaSrc   : [ "'self'" ],
+    frameSrc   : [ "'self'", '*.vimeo.com', '*.youtube.com' ]
+  }));
 
   if (config.app.favicon) {
     app.use(favicon(config.app.favicon));
